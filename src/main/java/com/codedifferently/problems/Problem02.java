c35ac3f144a7db0585435a1a333b2429e4d52c32
Boyce007 <diboyce21@students.desu.edu>
2022-07-06 15:29:11 -0400

    3:29

--- src/main/java/com/codedifferently/problems/Problem02.java
+++ src/main/java/com/codedifferently/problems/Problem02.java
@@ -6,16 +6,8 @@ import java.util.Collections;
 public class Problem02 {
     public static Integer cousinTag(ArrayList<String> cousins, Integer startIndex, String targetName){
         Integer targetIndex = cousins.indexOf(targetName);
-        if (startIndex == 0 && targetIndex == cousins.size()-1) {
-            return 1;
-        }
-        if (startIndex == 0 && targetIndex == cousins.size()-2) {
-            return 2;
-        }
-        if(targetIndex==cousins.size()/2) {
-            return cousins.size()/2;
-        }
-
-        return Math.abs(targetIndex-startIndex);
+        if (Math.abs(targetIndex - startIndex)  <Math.abs(cousins.size() - startIndex ))
+            return Math.abs(targetIndex-startIndex);
+        return cousins.size()-startIndex;
     }
 }
